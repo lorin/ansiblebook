c447cd4b9e49994a9d413edd037e08d703b859cf
Lorin Hochstein <lhochstein@netflix.com>
2016-10-03 22:17:23 -0700

    Set ALLOWED_HOSTS

--- ch05/mezzanine-dev.sh
+++ ch05/mezzanine-dev.sh
@@ -4,5 +4,6 @@ source venv/bin/activate
 pip install mezzanine
 mezzanine-project myproject
 cd myproject
+sed -i.bak 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ["127.0.0.1"]/' myproject/settings.py
 python manage.py createdb
 python manage.py runserver
