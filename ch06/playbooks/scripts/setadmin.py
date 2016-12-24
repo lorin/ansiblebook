b20a8d93030dbcb06b503b02ec1d3693cfbd8d75
Lorin Hochstein <lhochstein@netflix.com>
2016-12-23 19:11:46 -0800

    Update python scripts

--- ch06/playbooks/scripts/setadmin.py
+++ ch06/playbooks/scripts/setadmin.py
@@ -3,6 +3,7 @@
 # Assumes two environment variables
 #
 # PROJECT_DIR: the project directory (e.g., ~/projname)
+# PROJECT_APP: name of the project app
 # ADMIN_PASSWORD: admin user's password
 
 import os
@@ -12,10 +13,9 @@ import sys
 proj_dir = os.path.expanduser(os.environ['PROJECT_DIR'])
 sys.path.append(proj_dir)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = proj_app + '%.settings'
 
-
-from mezzanine.utils.models import get_user_model
+from django.contrib.auth import get_user_model
 User = get_user_model()
 u, _ = User.objects.get_or_create(username='admin')
 u.is_staff = u.is_superuser = True
