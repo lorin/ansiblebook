753086306d3d195b82799b0660610263edea3356
Lorin Hochstein <lhochstein@netflix.com>
2017-01-22 20:43:58 -0800

    Commit of mezznaine example files

---
+++ ch13/mezzanine_example/mezzanine_example/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mezzanine_example project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("mezzanine_example"))
+
+application = get_wsgi_application()
