4adf00d80704910f8b21696f42f3bbe53de2b496
Lorin Hochstein <lhochstein@netflix.com>
2017-02-05 14:30:41 -0800

    Create a config.js

--- ch13/ghostdata/config.js
+++ ch13/ghostdata/config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://localhost:8000',
         mail: {},
         database: {
             client: 'sqlite3',
